502f219ed312bff2ea7e400ed49208b35c85bac9
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-02-25 20:13:38 -0500

    database code to create tables created, some data imputed

--- seed.sql
+++ seed.sql
@@ -21,6 +21,44 @@
       first_name VARCHAR(30)
       last_name VARCHAR(30)
       role_id INT
-      manager_id INT 
+      manager_id INT */
 
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE DATABASE employee_trackerDB;
 
+USE employee_trackerDB;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO department (name)
+VALUES ("Fiance"), ("Engineering"), ("Sales"), ("Legal");
+-- 1. finance roles: 1 Accountant, 2 Account Manager
+-- 2. engineering roles: 3 Software Engineer, 4 Hardware Engineer
+-- 3. sales roles: 5 Sales Lead , 6 Sales Associate
+-- 4. legal roles: 7 Lawyer, 8 Paralegal 
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30), 
+    salary DECIMAL, 
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 60000, 1), ("Account Manager", 75000, 1), ("Software Engineer", 80000, 2), ("Hardware Engineer", 82000, 2), ("Sales Lead", 62000, 3), ("Sales Associate", 55000, 3), ("Lawyer", 73000, 4), ("Paralegal", 50000, 4); 
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Jessica", "Clark", 2);
